95c68686dc8656fa9384d49231180bd42edbf45c
BanariVlad <banari.vlad@yandex.com>
2020-06-17 14:17:43 +0300

    Ocean of ives

--- src/main.js
+++ src/main.js
@@ -12,15 +12,64 @@ Vue.directive("draggable", {
     let initialY;
     let dragElement = document.createElement("h2");
     dragElement.style.cursor = "move";
-    dragElement.innerText = "Drag me to move!";
+    dragElement.style.width = "100%";
+    dragElement.style.height = "20px";
+    dragElement.style.background = "#aeaeae";
     el.insertBefore(dragElement, el.firstChild);
     el.style.position = "absolute";
 
     const mouseMove = (e) => {
       let displayX = e.clientX - initialX;
       let displayY = e.clientY - initialY;
-      el.style.top = startY + displayY + "px";
-      el.style.left = startX + displayX + "px";
+      let offsetBottom = el.offsetTop + el.clientHeight;
+      let offsetRight = el.offsetLeft + el.clientWidth + 10;
+      let windowHeight = document.documentElement.clientHeight;
+      let windowWidth = document.documentElement.clientWidth;
+
+      if (el.offsetTop <= 0 && el.offsetLeft <= 0) {
+        if (displayX > 0 || displayY > 0) {
+          el.style.top = startY + displayY + "px";
+          el.style.left = startX + displayX + "px";
+        }
+      } else if (el.offsetLeft <= 0 && offsetBottom >= windowHeight) {
+        if (displayX > 0 || displayY < 0) {
+          el.style.top = startY + displayY + "px";
+          el.style.left = startX + displayX + "px";
+        }
+      } else if (offsetBottom >= windowHeight && offsetRight >= windowWidth) {
+        if (displayX < 0 || displayY < 0) {
+          el.style.top = startY + displayY + "px";
+          el.style.left = startX + displayX + "px";
+        }
+      } else if (el.offsetTop <= 0 && offsetRight >= windowWidth) {
+        if (displayX < 0 || displayY > 0) {
+          el.style.top = startY + displayY + "px";
+          el.style.left = startX + displayX + "px";
+        }
+      } else if (el.offsetTop <= 0) {
+        if (displayY > 0) {
+          el.style.top = startY + displayY + "px";
+        }
+        el.style.left = startX + displayX + "px";
+      } else if (el.offsetLeft <= 0) {
+        if (displayX > 0) {
+          el.style.left = startX + displayX + "px";
+        }
+        el.style.top = startY + displayY + "px";
+      } else if (offsetBottom >= windowHeight) {
+        if (displayY < 0) {
+          el.style.top = startY + displayY + "px";
+        }
+        el.style.left = startX + displayX + "px";
+      } else if (offsetRight >= windowWidth) {
+        if (displayX < 0) {
+          el.style.left = startX + displayX + "px";
+        }
+        el.style.top = startY + displayY + "px";
+      } else {
+        el.style.top = startY + displayY + "px";
+        el.style.left = startX + displayX + "px";
+      }
     }
 
     const mouseUp = () => {
