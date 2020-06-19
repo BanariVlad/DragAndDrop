6125e5b98cae0963277c6a52cf5c5800299b4652
BanariVlad <banari.vlad@yandex.com>
2020-06-19 18:52:14 +0300

    Save coordinates

--- src/main.js
+++ src/main.js
@@ -15,99 +15,114 @@ Vue.directive("draggable", {
     dragElement.style.width = "100%";
     dragElement.style.height = "20px";
     dragElement.style.background = "#aeaeae";
+    const blockCoordinates = localStorage.getItem("blockCoordinates");
+    const saveCoordinates = JSON.parse(blockCoordinates);
     el.insertBefore(dragElement, el.firstChild);
     el.style.position = "absolute";
+    if (saveCoordinates !== null) {
+      el.style.top = saveCoordinates.y;
+      el.style.left = saveCoordinates.x;
+    }
 
     const move = (displayX, displayY) => {
       el.style.top = startY + displayY + "px";
       el.style.left = startX + displayX + "px";
-    }
+    };
 
     const lockMoveToTop = (displayX, displayY) => {
       if (displayY > 0) {
         el.style.top = startY + displayY + "px";
       }
       el.style.left = startX + displayX + "px";
-    }
+    };
 
     const lockMoveToRight = (displayX, displayY) => {
       if (displayX < 0) {
         el.style.left = startX + displayX + "px";
       }
       el.style.top = startY + displayY + "px";
-    }
+    };
 
     const lockMoveToBottom = (displayX, displayY) => {
       if (displayY < 0) {
         el.style.top = startY + displayY + "px";
       }
       el.style.left = startX + displayX + "px";
-    }
+    };
 
     const lockMoveToLeft = (displayX, displayY) => {
       if (displayX > 0) {
         el.style.left = startX + displayX + "px";
       }
       el.style.top = startY + displayY + "px";
-    }
+    };
 
     const allowMoveForTopRight = (displayX, displayY) => {
       if (displayX < 0 || displayY > 0) {
         move(displayX, displayY);
       }
-    }
+    };
 
     const allowMoveForRightBottom = (displayX, displayY) => {
       if (displayX < 0 || displayY < 0) {
         move(displayX, displayY);
       }
-    }
+    };
 
     const allowMoveForBottomLeft = (displayX, displayY) => {
       if (displayX > 0 || displayY < 0) {
         move(displayX, displayY);
       }
-    }
+    };
 
     const allowMoveForLeftTop = (displayX, displayY) => {
       if (displayX > 0 || displayY > 0) {
         move(displayX, displayY);
       }
-    }
+    };
 
-    const checkPassed = (coordinates) => {
-      if (coordinates.top <= 0 && coordinates.right >= coordinates.windowWidth) {
-        return "topRight"
-      } else if (coordinates.right >= coordinates.windowWidth && coordinates.bottom >= coordinates.windowHeight) {
-        return "rightBottom"
-      } else if (coordinates.bottom >= coordinates.windowHeight && coordinates.left <= 0) {
-        return "bottomLeft"
+    const checkPassed = coordinates => {
+      if (
+        coordinates.top <= 0 &&
+        coordinates.right >= coordinates.windowWidth
+      ) {
+        return "topRight";
+      } else if (
+        coordinates.right >= coordinates.windowWidth &&
+        coordinates.bottom >= coordinates.windowHeight
+      ) {
+        return "rightBottom";
+      } else if (
+        coordinates.bottom >= coordinates.windowHeight &&
+        coordinates.left <= 0
+      ) {
+        return "bottomLeft";
       } else if (coordinates.left <= 0 && coordinates.top <= 0) {
-        return "leftTop"
+        return "leftTop";
       } else if (coordinates.top <= 0) {
         return "top";
       } else if (coordinates.right >= coordinates.windowWidth) {
-        return "right"
+        return "right";
       } else if (coordinates.bottom >= coordinates.windowHeight) {
-        return "bottom"
+        return "bottom";
       } else if (coordinates.left <= 0) {
-        return "left"
+        return "left";
       }
-    }
+    };
 
-    const fixIfPassed = (coordinates) => {
+    const fixIfPassed = coordinates => {
       if (coordinates.top + 10 < 10) {
-        el.style.top = "10px"
+        el.style.top = "10px";
       } else if (coordinates.right > coordinates.windowWidth) {
-        el.style.left = coordinates.windowWidth - el.clientWidth - 10 + "px"
+        el.style.left = coordinates.windowWidth - el.clientWidth - 10 + "px";
       } else if (coordinates.bottom > coordinates.windowHeight) {
-        el.style.top = coordinates.windowHeight - el.clientHeight - 10 + "px"
+        el.style.top = coordinates.windowHeight - el.clientHeight - 10 + "px";
       } else if (coordinates.left < 0) {
-        el.style.left = "10px"
+        el.style.left = "10px";
       }
-    }
+    };
 
-    const mouseMove = (e) => {
+    const mouseMove = e => {
       let displayX = e.clientX - initialX;
       let displayY = e.clientY - initialY;
       let coordinates = {
@@ -117,7 +132,7 @@ Vue.directive("draggable", {
         left: el.offsetLeft - 10,
         windowHeight: document.documentElement.clientHeight,
         windowWidth: document.documentElement.clientWidth
-      }
+      };
       let passedBorder = checkPassed(coordinates);
 
       switch (passedBorder) {
@@ -150,14 +165,22 @@ Vue.directive("draggable", {
           break;
       }
       fixIfPassed(coordinates);
-    }
+    };
 
     const mouseUp = () => {
       document.removeEventListener("mousemove", mouseMove);
       document.removeEventListener("mouseup", mouseUp);
-    }
+      let blockCoordinates = {
+        x: el.style.left,
+        y: el.style.top
+      };
+      localStorage.setItem(
+        "blockCoordinates",
+        JSON.stringify(blockCoordinates)
+      );
+    };
 
-    dragElement.addEventListener("mousedown", function (e) {
+    dragElement.addEventListener("mousedown", function(e) {
       startX = el.offsetLeft;
       startY = el.offsetTop;
       initialX = e.clientX;
@@ -166,7 +189,7 @@ Vue.directive("draggable", {
       document.addEventListener("mouseup", mouseUp);
     });
   }
-})
+});
 
 new Vue({
   router,
